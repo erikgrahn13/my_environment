3d478fdf6e4e381de30dd653b8df4012c760bea9
Erik Grahn <erik.grahn@aptiv.com>
2023-06-30 13:47:37 +0200

    install correct link

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)"
+
 
