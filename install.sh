7540d18f6ef9e3db2671de4a80d3ebeb95e3ecda
Erik Grahn <erik.grahn@aptiv.com>
2023-07-03 12:02:14 +0000

    update script

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# touch ~/.bashrc
+# echo "source /etc/skel/.bashrc" >> ~/.bashrc
+uid_gid_entrypoint
+
+cp /etc/skel/.bashrc ~/
 
 printf '2\nn\n' | sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh-bin/master/install)"
 
-export PATH="/home/builder/.local/bin:$PATH"
 
 printf 'Y\n' | sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
+echo "export PATH="/home/builder/.local/bin:\$PATH"" >> ~/.bashrc
+echo "exec zsh" >> ~/.bashrc
+# exh
 # exec zsh
 # sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)"
 
