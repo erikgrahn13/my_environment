b5874980c820a358d41828ae7b86dcb928d14b95
Erik Grahn <erik.grahn@aptiv.com>
2023-07-03 12:42:02 +0000

    install powerlevel10k

--- install.sh
+++ install.sh
@@ -14,6 +14,9 @@ echo "export PATH="/home/builder/.local/bin:\$PATH"" >> ~/.bashrc
 
 printf 'Y\n' | sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "ZSH_THEME="powerlevel10k/powerlevel10k"" >> ~/.zshrc
+
 echo "exec zsh" >> ~/.bashrc
 # exh
 # exec zsh
